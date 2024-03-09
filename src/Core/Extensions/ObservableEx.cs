016e642123c45daa4a04e4f9542b1b89a6172f77
LaughingLeader <laughingleader@gmail.com>
2024-03-09 10:10:24 -0600

    Add a workaround for StartAsync being blocking

---
+++ src/Core/Extensions/ObservableEx.cs
@@ -0,0 +1,19 @@
+﻿using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace DivinityModManager;
+public static class ObservableEx
+{
+	//public static IObservable<Unit> StartAsync(Func<CancellationToken, Task> actionAsync, IScheduler scheduler)
+
+	/// <summary>
+	/// Workaround for the fact that StartAsync is blocking.
+	/// See here:
+	/// https://github.com/dotnet/reactive/issues/457
+	/// </summary>
+	public static IObservable<Unit> CreateAndStartAsync(Func<CancellationToken, Task> func, IScheduler scheduler)
+	{
+		return Observable.Defer(() => Observable.StartAsync(func, scheduler)).SubscribeOn(scheduler);
+	}
+}
