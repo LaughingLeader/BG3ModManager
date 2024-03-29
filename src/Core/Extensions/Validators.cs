23b2e400bcfc6cd26c07ef2179af0cd4e624825e
LaughingLeader <laughingleader@gmail.com>
2024-03-29 01:56:53 -0500

    Add NotNullWhen atributes
    This allows the function to work as a "not null" check, for nullability.

--- src/Core/Extensions/Validators.cs
+++ src/Core/Extensions/Validators.cs
@@ -1,4 +1,6 @@
-﻿namespace ModManager;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ModManager;
 public static class Validators
 {
 	/// <summary>
@@ -6,12 +8,12 @@ public static class Validators
 	/// </summary>
 	/// <param name="str"></param>
 	/// <returns></returns>
-	public static bool IsValid(this Uri? uri) => !string.IsNullOrEmpty(uri?.AbsolutePath);
+	public static bool IsValid([NotNullWhen(true)] this Uri? uri) => !string.IsNullOrEmpty(uri?.AbsolutePath);
 
 	/// <summary>
 	/// Not null or empty.
 	/// </summary>
 	/// <param name="str"></param>
 	/// <returns></returns>
-	public static bool IsValid(this string? str) => !string.IsNullOrEmpty(str);
+	public static bool IsValid([NotNullWhen(true)] this string? str) => !string.IsNullOrEmpty(str);
 }
