1c39be82f344866bd1a0bef6b764317c1a248585
LaughingLeader <laughingleader@gmail.com>
2024-03-31 22:42:51 -0500

    Add a workaround for ComboBox context menu

---
+++ src/GUI/ModManager/Controls/ComboBoxWithRightClick.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+
+namespace ModManager.Controls;
+
+/// <summary>
+/// Allows right clicking a ComboBox to open a Flyout (as a custom context menu);
+/// The default behavior of ComboBox doesn't distinguish between click types - it just opens or closes on pointer pressed/released events.
+/// </summary>
+public class ComboBoxWithRightClick : ComboBox
+{
+	protected override Type StyleKeyOverride => typeof(ComboBox);
+
+	protected override void OnPointerPressed(PointerPressedEventArgs e)
+	{
+		var props = e.GetCurrentPoint(this).Properties;
+		if (props.PointerUpdateKind == PointerUpdateKind.RightButtonPressed)
+		{
+			e.Handled = true;
+			return;
+		}
+		base.OnPointerPressed(e);
+	}
+
+	protected override void OnPointerReleased(PointerReleasedEventArgs e)
+	{
+		var props = e.GetCurrentPoint(this).Properties;
+		if (props.PointerUpdateKind == PointerUpdateKind.RightButtonReleased)
+		{
+			e.Handled = true;
+			FlyoutBase.ShowAttachedFlyout(this);
+			return;
+		}
+		base.OnPointerReleased(e);
+	}
+}
--- src/GUI/ModManager/Views/Main/MainCommandBar.axaml.cs
+++ src/GUI/ModManager/Views/Main/MainCommandBar.axaml.cs
@@ -14,14 +14,6 @@ public partial class MainCommandBar : ReactiveUserControl<MainCommandBarViewMode
 
 		//TODO See if there's a way to fix FAComboBox staying "pressed" when opening a context menu
 
-		OrdersComboBox.PointerPressed += (o, e) =>
-		{
-			if (o is Visual v && e.GetCurrentPoint(v).Properties.IsRightButtonPressed)
-			{
-				e.Handled = true;
-			}
-		};
-
 		Observable.FromEventPattern<PointerPressedEventArgs>(OrdersComboBox, nameof(OrdersComboBox.PointerPressed))
 		.Subscribe(x =>
 		{
@@ -31,14 +23,15 @@ public partial class MainCommandBar : ReactiveUserControl<MainCommandBarViewMode
 			}
 		});
 
-		Observable.FromEventPattern<ContextRequestedEventArgs>(OrdersComboBox, nameof(OrdersComboBox.ContextRequested))
+		/*Observable.FromEventPattern<ContextRequestedEventArgs>(OrdersComboBox, nameof(OrdersComboBox.ContextRequested))
 		.Subscribe(x =>
 		{
 			if (x.Sender is Control target && Resources.TryGetValue("RenamingCommandBarFlyout", out var obj) && obj is CommandBarFlyout flyout)
 			{
 				flyout.ShowAt(target, false);
 				x.EventArgs.Handled = true;
+				OrdersComboBox.IsDropDownOpen = false;
 			}
-		});
+		});*/
 	}
 }
