8dbd28dddd4b1cbe9ae91c24ecf378326f664887
LaughingLeader <laughingleader@gmail.com>
2024-04-06 09:49:28 -0500

    Switch to a circular progress bar

--- src/GUI/ModManager/Views/Main/ProgressBarView.axaml.cs
+++ src/GUI/ModManager/Views/Main/ProgressBarView.axaml.cs
@@ -1,4 +1,6 @@
+using Avalonia.Animation;
 using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
 
 using ModManager.ViewModels.Main;
 
@@ -9,12 +11,23 @@ public partial class ProgressBarView : ReactiveUserControl<ProgressBarViewModel>
 	{
 		InitializeComponent();
 
+		ProgressBarControl.Value = 0d;
+
 		this.WhenActivated(d =>
 		{
 			this.GetObservable(IsVisibleProperty).BindTo(ViewModel, x => x.IsVisible);
+
+			/*var arc = ProgressBarControl.FindControl<Arc>("PART_ArcFill");
+			if(arc != null)
+			{
+				var transition = arc.Transitions.OfType<DoubleTransition>().FirstOrDefault();
+			}*/
+
 			ViewModel.WhenAnyValue(x => x.Title).ObserveOn(RxApp.MainThreadScheduler).BindTo(this, x => x.TitleTextControl.Text);
 			ViewModel.WhenAnyValue(x => x.WorkText).ObserveOn(RxApp.MainThreadScheduler).BindTo(this, x => x.WorkTextControl.Text);
-			ViewModel.WhenAnyValue(x => x.Value).ObserveOn(RxApp.MainThreadScheduler).BindTo(this, x => x.ProgressBarControl.Value);
+			var whenValue = ViewModel.WhenAnyValue(x => x.Value).ObserveOn(RxApp.MainThreadScheduler);
+			whenValue.BindTo(this, x => x.ProgressBarControl.Value);
+			whenValue.Select(x => $"{x:0}%").BindTo(this, x => x.ProgressValueTextControl.Text);
 		});
 	}
 }
