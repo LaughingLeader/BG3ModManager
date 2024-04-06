f7ad175d292334885870f4da321e82aed7fd58b6
LaughingLeader <laughingleader@gmail.com>
2024-04-06 12:57:14 -0500

    Preserve selection when drag n' dropping

--- src/GUI/ModManager/Views/Mods/ModListView.axaml.cs
+++ src/GUI/ModManager/Views/Mods/ModListView.axaml.cs
@@ -1,6 +1,10 @@
+using Avalonia.Controls.Models.TreeDataGrid;
 using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Selection;
 using Avalonia.Input;
 using Avalonia.LogicalTree;
+
+using DynamicData;
 
 using ModManager.Models.Mod;
 using ModManager.ViewModels.Mods;
@@ -8,6 +12,11 @@ using ModManager.ViewModels.Mods;
 namespace ModManager.Views.Mods;
 public partial class ModListView : ReactiveUserControl<ModListViewModel>
 {
+	/// <summary>
+	/// Janky, but this is used to preserve selections when drag+drop reordering mods.
+	/// </summary>
+	private HashSet<string> _lastSelected = [];
+
 	/// <summary>
 	/// Prevents dropping mods into other mods, and instead makes the drop operation move the mod in the list.
 	/// Only ModCategory models should allow dropping mods into them.
@@ -20,6 +29,25 @@ public partial class ModListView : ReactiveUserControl<ModListViewModel>
 			//e.Inner.DragEffects = DragDropEffects.None;
 			e.Position = TreeDataGridRowDropPosition.Before;
 		}
+	}
+
+	private void OnDrag(TreeDataGridRowDragEventArgs e)
+	{
+		MaybeRedirectDrop(e);
+		_lastSelected.Clear();
+		if (e.Inner.Data.Get(DragInfo.DataFormat) is DragInfo di && di.Source.Selection is ITreeDataGridRowSelectionModel<IModEntry> selection)
+		{
+			var mods = selection.SelectedItems;
+			foreach (var mod in mods)
+			{
+				if (mod?.UUID.IsValid() == true) _lastSelected.Add(mod.UUID);
+			}
+		}
+	}
+
+	private void OnDrop(TreeDataGridRowDragEventArgs e)
+	{
+		MaybeRedirectDrop(e);
 	}
 
 	public ModListView()
@@ -41,19 +69,28 @@ public partial class ModListView : ReactiveUserControl<ModListViewModel>
 				h => (sender, e) => h(e),
 				h => ModsTreeDataGrid.RowDragOver += h,
 				h => ModsTreeDataGrid.RowDragOver -= h
-			).Subscribe(MaybeRedirectDrop));
+			).Subscribe(OnDrag));
 
 			d(Observable.FromEvent<EventHandler<TreeDataGridRowDragEventArgs>?, TreeDataGridRowDragEventArgs>(
 				h => (sender, e) => h(e),
 				h => ModsTreeDataGrid.RowDrop += h,
 				h => ModsTreeDataGrid.RowDrop -= h
-			).Subscribe(MaybeRedirectDrop));
+			).Subscribe(OnDrop));
 
-			d(Observable.FromEventPattern<ChildIndexChangedEventArgs>(ModsTreeDataGrid.RowsPresenter!, nameof(ModsTreeDataGrid.RowsPresenter.ChildIndexChanged)).Subscribe(e =>
+			d(Observable.FromEventPattern<ChildIndexChangedEventArgs>(ModsTreeDataGrid.RowsPresenter!, 
+				nameof(ModsTreeDataGrid.RowsPresenter.ChildIndexChanged)).Subscribe(e =>
 			{
 				if(e.EventArgs.Child is TreeDataGridRow row && row.Model is IModEntry mod)
 				{
 					mod.Index = e.EventArgs.Index;
+					if (mod.UUID.IsValid() && _lastSelected.Contains(mod.UUID))
+					{
+						_lastSelected.Remove(mod.UUID);
+						RxApp.MainThreadScheduler.Schedule(TimeSpan.FromMilliseconds(10), () =>
+						{
+							ModsTreeDataGrid.RowSelection!.Select(mod.Index);
+						});
+					}
 				}
 			}));
 			
