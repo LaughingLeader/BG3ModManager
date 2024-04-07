f662fbcc906ac57242fccd7b273dfe81f3afd4e9
LaughingLeader <laughingleader@gmail.com>
2024-04-06 20:10:35 -0500

    Swap back to a ReactiveWindow
    SukiWindow's title bar scales kind of weird, compared to the default window title bar.

--- src/GUI/ModManager/Windows/MainWindow.axaml.cs
+++ src/GUI/ModManager/Windows/MainWindow.axaml.cs
@@ -10,11 +10,8 @@ using SukiUI.Controls;
 
 namespace ModManager.Windows;
 
-public partial class MainWindow : SukiWindow, IViewFor<MainWindowViewModel>
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
-	public static readonly StyledProperty<MainWindowViewModel?> ViewModelProperty = AvaloniaProperty
-			.Register<MainWindow, MainWindowViewModel?>(nameof(ViewModel));
-
 	public void ShowSukiDialog(object? content, bool showCardBehind = true, bool allowBackgroundClose = false)
 	{
 		var host = this.FindDescendantOfType<SukiHost>() ?? throw new InvalidOperationException("No SukiHost present in this window");
@@ -50,12 +47,6 @@ public partial class MainWindow : SukiWindow, IViewFor<MainWindowViewModel>
 	public MainWindow()
 	{
 		InitializeComponent();
-
-		// This WhenActivated block calls ViewModel's WhenActivated
-		// block if the ViewModel implements IActivatableViewModel.
-		this.WhenActivated(disposables => { });
-		this.GetObservable(DataContextProperty).Subscribe(OnDataContextChanged);
-		this.GetObservable(ViewModelProperty).Subscribe(OnViewModelChanged);
 
 		this.WhenActivated(d =>
 		{
@@ -93,44 +84,5 @@ public partial class MainWindow : SukiWindow, IViewFor<MainWindowViewModel>
 				}, RxApp.TaskpoolScheduler);
 			});
 		});
-	}
-
-	/// <summary>
-	/// The ViewModel.
-	/// </summary>
-	public MainWindowViewModel? ViewModel
-	{
-		get => GetValue(ViewModelProperty);
-		set => SetValue(ViewModelProperty, value);
-	}
-
-	object? IViewFor.ViewModel
-	{
-		get => ViewModel;
-		set => ViewModel = (MainWindowViewModel?)value;
-	}
-
-	private void OnDataContextChanged(object? value)
-	{
-		if (value is MainWindowViewModel viewModel)
-		{
-			ViewModel = viewModel;
-		}
-		else
-		{
-			ViewModel = null;
-		}
-	}
-
-	private void OnViewModelChanged(object? value)
-	{
-		if (value == null)
-		{
-			ClearValue(DataContextProperty);
-		}
-		else if (DataContext != value)
-		{
-			DataContext = value;
-		}
 	}
 }
