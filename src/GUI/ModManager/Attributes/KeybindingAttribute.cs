983636b7dac614b80b34ccaadc45310bd393d21d
LaughingLeader <laughingleader@gmail.com>
2024-04-14 19:49:32 -0500

    Add optional tooltip property

--- src/GUI/ModManager/Attributes/KeybindingAttribute.cs
+++ src/GUI/ModManager/Attributes/KeybindingAttribute.cs
@@ -4,16 +4,20 @@ namespace ModManager;
 public class KeybindingAttribute : Attribute
 {
 	public string? DisplayName { get; set; }
+	public string? ToolTip { get; set; }
 	public string? MenuCategory { get; set; }
 
 	public Key Key { get; set; }
 	public ModifierKeys Modifiers { get; set; }
 
-	public KeybindingAttribute(string displayName, Key key, ModifierKeys modifiers = KeyModifiers.None, string? menuCategory = null)
+	public KeybindingAttribute(string displayName, Key key, ModifierKeys modifiers = KeyModifiers.None, string? tooltip = null, string? menuCategory = null)
 	{
 		DisplayName = displayName;
 		Key = key;
 		Modifiers = modifiers;
+		ToolTip = tooltip;
 		MenuCategory = menuCategory;
 	}
+
+	public KeybindingAttribute(){ }
 }
