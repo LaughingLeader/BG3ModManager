24940155ba9d989b87d1ffb98dbaabcc5922c1d8
LaughingLeader <laughingleader@gmail.com>
2024-06-19 15:00:09 -0500

    Generate keybinding registration

--- src/GUI/ModManager/ViewModels/Main/MainCommandBarViewModel.cs
+++ src/GUI/ModManager/ViewModels/Main/MainCommandBarViewModel.cs
@@ -10,24 +10,59 @@ public partial class MainCommandBarViewModel : ReactiveObject
 
 	[Keybinding("Check For App Updates", Key.U)]
 	public RxCommandUnit? CheckForAppUpdatesCommand { get; set; }
+
 	public RxCommandUnit? CheckForGitHubModUpdatesCommand { get; set; }
+
 	public RxCommandUnit? CheckForNexusModsUpdatesCommand { get; set; }
+
 	public RxCommandUnit? CheckForSteamWorkshopUpdatesCommand { get; set; }
+
+	[Keybinding("Export Order to Archive As...", Key.R, KeyModifiers.Control | KeyModifiers.Shift, "Export all active mods to an archive file of a chosen type", "File")]
 	public RxCommandUnit? ExportModsToZipAsCommand { get; set; }
+
+	[Keybinding("Export Order to Archive (.zip)", Key.R, KeyModifiers.Control, "Export all active mods to zip", "File")]
 	public RxCommandUnit? ExportModsToZipCommand { get; set; }
+
+	[Keybinding("Export Order to Game", Key.E, KeyModifiers.Control, "Export order to modsettings.lsx", "File")]
 	public ReactiveCommand<Unit,bool>? ExportOrderCommand { get; set; }
+
+	[Keybinding("Launch Game", Key.G, KeyModifiers.Control, "", "Go")]
 	public RxCommandUnit? LaunchGameCommand { get; set; }
+
+	[Keybinding("Donate a Coffee...", Key.F10, KeyModifiers.None, "", "Help")]
 	public RxCommandUnit? OpenDonationPageCommand { get; set; }
+
+	[Keybinding("Open Repository Page...", Key.F11, KeyModifiers.None, "", "Help")]
 	public RxCommandUnit? OpenGitHubRepoCommand { get; set; }
+
+	[Keybinding("Open Mods Folder", Key.D1, KeyModifiers.Control, "", "Go")]
 	public RxCommandUnit? OpenModsFolderCommand { get; set; }
+
+	[Keybinding("Open Nexus Mods Website", Key.D3, KeyModifiers.Control, "", "Go")]
 	public RxCommandUnit? OpenNexusModsCommand { get; set; }
+
+	[Keybinding("Open Steam Store Page", Key.D4, KeyModifiers.Control, "", "Go")]
 	public RxCommandUnit? OpenSteamPageCommand { get; set; }
+
+	[Keybinding("Refresh Mods", Key.F5, KeyModifiers.None, "", "File")]
 	public RxCommandUnit? RefreshCommand { get; set; }
+
+	[Keybinding("Refresh Mod Updates", Key.F6, KeyModifiers.None, "", "File")]
 	public RxCommandUnit? RefreshModUpdatesCommand { get; set; }
+
+	[Keybinding("Rename Save", Key.None, KeyModifiers.None, "", "File")]
 	public RxCommandUnit? RenameSaveCommand { get; set; }
+
+	[Keybinding("Save Order", Key.S, KeyModifiers.Control, "", "File")]
 	public ReactiveCommand<Unit, bool>? SaveOrderCommand { get; set; }
+
+	[Keybinding("Save Order As...", Key.S, KeyModifiers.Control | KeyModifiers.Alt, "", "File")]
 	public ReactiveCommand<Unit, bool>? SaveOrderAsCommand { get; set; }
+
+	[Keybinding("Save Settings", Key.S, KeyModifiers.Control | KeyModifiers.Shift, "", "File")]
 	public RxCommandUnit? SaveSettingsSilentlyCommand { get; set; }
+
+	[Keybinding("Toggle Updates View", Key.U, KeyModifiers.Control | KeyModifiers.Alt, "", "View")]
 	public RxCommandUnit? ToggleUpdatesViewCommand { get; set; }
 
 	public void CreateCommands(MainWindowViewModel main, ModOrderViewModel modOrder)
@@ -90,7 +125,7 @@ public partial class MainCommandBarViewModel : ReactiveObject
 			}
 		}, canToggleUpdatesView);
 
-		//this.RegisterKeybindings();
+		this.RegisterKeybindings();
 	}
 
 	public MainCommandBarViewModel()
